b2d46c944587334090cb91f86760242c8acc93c5
h-casticimo <hamiltoncasticimo17@gmail.com>
2022-11-27 15:35:57 +0800

    Created Portfolio
    created portfolio files

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Header from './elements/header/Header'
+import Nav from './elements/nav/Nav'
+import About from './elements/about/About'
+import Experiences from './elements/experiences/Experiences'
+import Projects from './elements/projects/Projects'
+import Contact from './elements/contact/Contact'
+import Footer from './elements/footer/Footer'
+
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Nav />
+            <About />
+            <Experiences />
+            <Projects />
+            <Contact />
+            <Footer />
+        </>
+    )
+}
+
+export default App
