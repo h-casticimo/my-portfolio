6ba75a6ff2392d583805fc915b46ab072ca1f6f7
h-casticimo <116869637+h-casticimo@users.noreply.github.com>
2023-03-17 15:19:04 +0000

    Updates

---
+++ precache-manifest.822ec2bfb450d9299e7f1475a9345328.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d8a706a264d7b8cc4c2",
+    "url": "/my-portfolio/static/css/main.43e491cb.chunk.css"
+  },
+  {
+    "revision": "2d8a706a264d7b8cc4c2",
+    "url": "/my-portfolio/static/js/main.2d8a706a.chunk.js"
+  },
+  {
+    "revision": "82b4ee65227d2eb0a7fd",
+    "url": "/my-portfolio/static/js/1.82b4ee65.chunk.js"
+  },
+  {
+    "revision": "f2bb3e8d5477afa4b7c6",
+    "url": "/my-portfolio/static/js/runtime~main.f2bb3e8d.js"
+  },
+  {
+    "revision": "c07e64cc95a6d17949095366148350bc",
+    "url": "/my-portfolio/static/media/me.c07e64cc.png"
+  },
+  {
+    "revision": "f6676e7efcf4c786bc172bddff7d0ea7",
+    "url": "/my-portfolio/static/media/FCC_RWD.f6676e7e.png"
+  },
+  {
+    "revision": "a0e57285d6bbefa1fc114a4d85f9d877",
+    "url": "/my-portfolio/static/media/LinuxAd.a0e57285.png"
+  },
+  {
+    "revision": "359521b9002896a55fd8bb821a52fe1e",
+    "url": "/my-portfolio/static/media/tipic.359521b9.jpg"
+  },
+  {
+    "revision": "edcdd16eb6c48bdbea25e32248e3d068",
+    "url": "/my-portfolio/static/media/thesis.edcdd16e.jpg"
+  },
+  {
+    "revision": "18214184f5138700cbbf98f3c8d1af31",
+    "url": "/my-portfolio/static/media/OrderForm.18214184.jpg"
+  },
+  {
+    "revision": "785f13a87281c2ed7ff09822c928c807",
+    "url": "/my-portfolio/static/media/TributePage.785f13a8.jpg"
+  },
+  {
+    "revision": "018b183d3cc7d5389c4367c92312a5e6",
+    "url": "/my-portfolio/static/media/portfolio_pic.018b183d.png"
+  },
+  {
+    "revision": "e3577ddd019c610cedbeb38ebff4e2ef",
+    "url": "/my-portfolio/static/media/texture-dark-wood.e3577ddd.png"
+  },
+  {
+    "revision": "4fe04431090043e441e38de006dc80c6",
+    "url": "/my-portfolio/index.html"
+  }
+];
